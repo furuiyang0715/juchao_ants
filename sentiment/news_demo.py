bc5a8b9f0fa5991fce42b19e7a886261b369caed
furuiyang <furuiyang0715@gmail.com>
2021-02-26 09:07:32 +0800

    股吧事件表 建表

--- sentiment/news_demo.py
+++ sentiment/news_demo.py
@@ -50,17 +50,48 @@ CREATE TABLE `dc_ann_event_source_news_detail` (
 
 
 sl_old = '''
-
-
-
-
+CREATE TABLE `dc_ann_event_source_guba_detail` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `GubaID` bigint(20) NOT NULL COMMENT '股吧主表ID',
+  `PubTime` datetime NOT NULL COMMENT '发布时间（精确到秒）',
+  `Title` varchar(500) COLLATE utf8_bin DEFAULT NULL COMMENT '标题',
+  `Website` varchar(1000) COLLATE utf8_bin DEFAULT NULL COMMENT '网址',
+  `SecuCode` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '股票代码',
+  `EventCode` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '事件代码',
+  `EventName` varchar(1000) COLLATE utf8_bin DEFAULT NULL COMMENT '事件名称',
+  `Position` tinyint(4) NOT NULL COMMENT '提及位置：1-标题,2-内容',
+  `IsValid` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否有效',
+  `CreateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `UpdateTime` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `un1` (`GubaID`,`SecuCode`,`EventCode`),
+  KEY `k1` (`GubaID`),
+  KEY `k2` (`PubTime`),
+  KEY `k3` (`SecuCode`),
+  KEY `k4` (`EventCode`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='公告舆情事件明细-股吧源'; 
 '''
 
 
 sl_new = '''
-
-
-
-
-
+CREATE TABLE `dc_ann_event_source_guba_detail` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
+  `guba_id` bigint(20) NOT NULL COMMENT '股吧主表ID',
+  `pub_time` datetime NOT NULL COMMENT '发布时间（精确到秒）',
+  `title` varchar(500) COLLATE utf8_bin DEFAULT NULL COMMENT '标题',
+  `web_site` varchar(1000) COLLATE utf8_bin DEFAULT NULL COMMENT '网址',
+  `secu_code` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '股票代码',    # secu_id 
+  `event_code` varchar(20) COLLATE utf8_bin DEFAULT NULL COMMENT '事件代码',
+  `event_name` varchar(1000) COLLATE utf8_bin DEFAULT NULL COMMENT '事件名称',
+  `position` tinyint(4) NOT NULL COMMENT '提及位置：1-标题,2-内容',
+  `is_valid` tinyint(4) NOT NULL DEFAULT '1' COMMENT '是否有效',
+  `create_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP COMMENT '创建时间',
+  `update_time` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT '更新时间',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `un1` (`guba_id`,`secu_code`,`event_code`),
+  KEY `k1` (`guba_id`),
+  KEY `k2` (`pub_time`),
+  KEY `k3` (`secu_code`),
+  KEY `k4` (`event_code`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin COMMENT='公告舆情事件明细-股吧源'; 
 '''
