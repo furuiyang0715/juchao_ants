4b1b2571b084fdfcc00ba1bd51c25aafa553780a
furuiyang <furuiyang0715@gmail.com>
2021-03-08 16:00:59 +0800

    add pool

---
+++ sql_pool.py
@@ -0,0 +1,216 @@
+import logging
+import traceback
+
+import pymysql
+from pymysql.cursors import DictCursor
+from DBUtils.PooledDB import PooledDB
+
+logger = logging.getLogger()
+
+
+class PyMysqlPoolBase(object):
+    """
+        建立 mysql 连接池的 python 封装
+        与直接建立 mysql 连接的不同之处在于连接在完成操作之后不会立即提交关闭
+        可复用以提高效率
+    """
+    _pool = None
+
+    def __init__(self,
+                 host,
+                 port,
+                 user,
+                 password,
+                 db=None, init=True):
+        self.db_host = host
+        self.db_port = int(port)
+        self.user = user
+        self.password = str(password)
+        self.db = db
+        self.connection = None
+        self.cursor = None
+        if init:
+            self.init
+
+    @property
+    def init(self):
+        self._getConn
+
+    @property
+    def _getConn(self):
+        """
+        @summary: 静态方法，从连接池中取出连接
+        @return MySQLdb.connection
+        """
+        if PyMysqlPoolBase._pool is None:
+            _pool = PooledDB(creator=pymysql,  # 用以链接数据库的模块
+                             mincached=1,  # 初始化时，连接池中至少创建的空闲链接 0 表示不创建
+                             maxcached=20,  # 链接池中最多闲置的链接，0和None不限制
+                             host=self.db_host,
+                             port=self.db_port,
+                             user=self.user,
+                             passwd=self.password,
+                             db=self.db,
+                             use_unicode=True,
+                             charset="utf8",
+                             cursorclass=DictCursor)
+        self.connection = _pool.connection()
+
+    def _exec_sql(self, sql, param=None):
+        with self.connection.cursor() as cursor:
+            if param is None:
+                count = cursor.execute(sql)
+            else:
+                count = cursor.execute(sql, param)
+        return count
+
+    def insert(self, sql, params=None):
+        """
+        @summary: 更新数据表记录
+        @param sql: SQL 格式及条件，使用 (%s,%s)
+        @param params: 要更新的值: tuple/list
+        @return: count 受影响的行数
+        """
+        return self._exec_sql(sql, params)
+
+    def select_all(self, sql, params=None):
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, params)
+            results = cursor.fetchall()
+        return results
+
+    def select_many(self, sql, params=None, size=1):
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, params)
+            results = cursor.fetchmany(size)
+        return results
+
+    def select_one(self, sql, params=None):
+        with self.connection.cursor() as cursor:
+            cursor.execute(sql, params)
+            result = cursor.fetchone()
+        return result
+
+    def insert_many(self, sql, values):
+        """
+        @summary: 向数据表插入多条记录
+        @param sql:要插入的 SQL 格式
+        @param values:要插入的记录数据tuple(tuple)/list[list]
+        @return: count 受影响的行数
+        """
+        with self.connection.cursor() as cursor:
+            count = cursor.executemany(sql, values)
+        return count
+
+    def update(self, sql, param=None):
+        """
+        @summary: 更新数据表记录
+        @param sql: SQL 格式及条件，使用(%s,%s)
+        @param param: 要更新的  值 tuple/list
+        @return: count 受影响的行数
+        """
+        return self._exec_sql(sql, param)
+
+    def delete(self, sql, param=None):
+        """
+        @summary: 删除数据表记录
+        @param sql: SQL 格式及条件，使用(%s,%s)
+        @param param: 要删除的条件 值 tuple/list
+        @return: count 受影响的行数
+        """
+        return self._exec_sql(sql, param)
+
+    def begin(self):
+        """
+        @summary: 开启事务
+        """
+        self.connection.autocommit(0)
+
+    def end(self, option='commit'):
+        """
+        @summary: 结束事务
+        """
+        if option == 'commit':
+            self.connection.commit()
+        else:
+            self.connection.rollback()
+
+    def dispose(self, isEnd=1):
+        """
+        @summary: 释放连接池资源
+        """
+        if isEnd == 1:
+            self.end('commit')
+        else:
+            self.end('rollback')
+        self.connection.close()
+
+    # 基于对象的插入
+    def contract_sql(self, datas, table: str, update_fields: list):
+        """拼接 sql 语句"""
+        if not isinstance(datas, list):
+            datas = [datas, ]
+
+        to_insert = datas[0]
+        ks = []
+        vs = []
+        for k in to_insert:
+            ks.append(k)
+            vs.append(to_insert.get(k))
+        fields_str = "(" + ",".join(ks) + ")"
+        values_str = "(" + "%s," * (len(vs) - 1) + "%s" + ")"
+        base_sql = '''INSERT INTO `{}` '''.format(table) + fields_str + ''' values ''' + values_str
+
+        params = []
+        for data in datas:
+            vs = []
+            for k in ks:
+                vs.append(data.get(k))
+            params.append(vs)
+
+        if update_fields:
+            # https://stackoverflow.com/questions/12825232/python-execute-many-with-on-duplicate-key-update/12825529#12825529
+            # sql = 'insert into A (id, last_date, count) values(%s, %s, %s) on duplicate key update last_date=values(last_date),count=count+values(count)'
+            on_update_sql = ''' ON DUPLICATE KEY UPDATE '''
+            for update_field in update_fields:
+                on_update_sql += '{}=values({}),'.format(update_field, update_field)
+            on_update_sql = on_update_sql.rstrip(",")
+            sql = base_sql + on_update_sql + """;"""
+        else:
+            sql = base_sql + ";"
+        return sql, params
+
+    def _batch_save(self, to_inserts, table, update_fields):
+        """批量插入"""
+        if len(to_inserts) == 0:
+            logger.warning("批量插入数据量为 0")
+            return 0
+        try:
+            sql, values = self.contract_sql(to_inserts, table, update_fields)
+            count = self.insert_many(sql, values)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            logger.info("批量插入的数量是{}".format(count))
+            self.end()
+            return count
+
+    def _save(self, to_insert, table, update_fields):
+        """单个插入"""
+        try:
+            insert_sql, values = self.contract_sql(to_insert, table, update_fields)
+            value = values[0]
+            count = self.insert(insert_sql, value)
+        except:
+            traceback.print_exc()
+            logger.warning("失败")
+        else:
+            if count == 1:
+                logger.info("插入新数据 {}".format(to_insert))
+            elif count == 2:
+                logger.info("刷新数据 {}".format(to_insert))
+            else:
+                logger.info("已有数据 {} ".format(to_insert))
+            self.end()
+            return count
