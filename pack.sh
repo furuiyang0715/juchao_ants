e7fb1d737c399385166b2c80a8557c3a8ccc8fe5
lixiongfei <3088185518@qq.com>
2021-03-10 15:32:27 +0800

    公告爬虫部署脚本

--- pack.sh
+++ pack.sh
@@ -1,20 +1,40 @@
-#!/bin/sh
-if [ ! -d out  ];then
-    mkdir out
+#!/bin/bash
+PWD=$(pwd)
+PROJECT="juchao_ant"
+# copy target file
+PACK_PATH="$PWD/target/$PROJECT"
+if [ ! -d $PACK_PATH  ];then
+    mkdir -p $PACK_PATH
 fi
 
-if [ -d tmp_conf ];then
-    rm -rf tmp_conf
+TARGET_FILE=$PWD/out/$PROJECT.tar.gz
+# create target pack file
+if [ ! -d $PWD/out  ];then
+    mkdir -p $PWD/out
+fi
+if [ -f TARGET_FILE ];then
+	rm -f $TARGET_FILE
 fi
 
-rm -f out/juchao_ant.tar.gz
+cp -rf ./*.py $PACK_PATH
+cp -rf annversion1 $PACK_PATH
+cp -rf annversion2 $PACK_PATH
+cp -rf spy_announcement $PACK_PATH
+cp requirements.txt $PACK_PATH
+VERSION_FILE=version
+git log --pretty=oneline -n 1 > $PACK_PATH/$VERSION_FILE
 
-# tar zxvf hkland_data_pro.tar.gz
-cd .. && (tar zcvf juchao_ant/out/hkland_data_pro.tar.gz \
-          --exclude=juchao_ant/out \
-          --exclude=*temp \
-          --exclude=*.pyc \
-          --exclude=*.git \
-          --exclude=*.idea \
-          --exclude=*__pycache__ \
-          juchao_ant)
+cd $PACK_PATH
+tar -zcvf $TARGET_FILE ../$PROJECT \
+                    --exclude=*.d \
+                    --exclude=*.o \
+                    --exclude=*/.git\
+                    --exclude=*/logs \
+                    --exclude=*.gitignore \
+                    --exclude=*.ini \
+                    --exclude=*.conf \
+                    --exclude=*.heap \
+                    --exclude=*.pdf \
+                    --exclude=*.ldb \
+                    --exclude=*.gitmodules
+rm -rf $PACK_PATH
